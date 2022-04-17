de21046f585934466339660684fb007f9876efce
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-04-17 14:11:39 +0600

    adding register

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Services from '../Services/Services/Services';
 
 const Home = () => {
     return (
         <div>
-            <h2>this is home</h2>
-
+            <Services></Services>
         </div>
     );
 };
