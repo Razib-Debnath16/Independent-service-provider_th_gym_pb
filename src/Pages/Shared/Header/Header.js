9d7e723266923cf492057b7014c57bd3cbe6fe56
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-04-17 12:40:32 +0600

    adding register

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,15 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const logout = () => {
+        signOut(auth);
+    };
     return (
         <Navbar collapseOnSelect expand="lg" bg="primary" sticky='top' variant="dark">
             <Container>
@@ -15,12 +22,13 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <Nav.Link style={{ color: 'white' }} as={Link} to="/about">About Me</Nav.Link>
-                        {/* {user ?
+                        {user ?
                             <Button onClick={logout} variant="primary">Log out</Button>
-                            : */}
-                        <Nav.Link style={{ color: 'white' }} as={Link} to="/Login">
-                            Login
-                        </Nav.Link>
+                            :
+                            <Nav.Link style={{ color: 'white' }} as={Link} to="/Login">
+                                Login
+                            </Nav.Link>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
