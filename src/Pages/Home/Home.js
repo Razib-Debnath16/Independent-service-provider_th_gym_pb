60039b2c498423c6877de687ba4c3ac5defbeceb
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-04-18 10:40:22 +0600

    adding footer on home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Reviews from '../Reviews/Reviews';
 import Services from '../Services/Services/Services';
+import Footer from '../Shared/Footer/Footer';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Services></Services>
             <Reviews></Reviews>
+            <Footer></Footer>
         </div>
     );
 };
